823262acb964237041117e58d6b392a74f9c67a5
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-03 15:24:20 +0000

    monty main.c optimized adding open_and_read() function

--- main.c
+++ main.c
@@ -8,24 +8,8 @@
  */
 int main(int argc, char **argv)
 {
-	FILE *fp;
-	char *line_buf = NULL, *token = NULL;
-	size_t num_ch_readed = 0;
-	unsigned int line_counter = 1;
-
 	if (argc != 2)
 		error_arguments();
-
-	fp = fopen(argv[1], "r");
-
-	if (fp == NULL)
-		open_error(argv);
-	while(getline(&line_buf, &num_ch_readed, fp) != EOF)
-	{
-		token = strtok(line_buf, " \n");
-		line_counter++;
-	}
-
-
+	open_and_read(argv);
 	return (0);
 }
