41502a0d85b36ddac0810a4c3891219a8fc3938f
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-04 22:43:56 +0000

    monty pop betty fix

--- stack.c
+++ stack.c
@@ -96,8 +96,8 @@ void pint_stack(stack_t **top, unsigned int line_number)
 void pop_stack(stack_t **top, unsigned int line_number)
 {
 	stack_t *tmp;
-	tmp = *top;
 
+	tmp = *top;
 	if (*top == NULL)
 		pop_error(line_number);
 
