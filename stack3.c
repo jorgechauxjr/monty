52b7659a983be1c58d664bd6c6c4640bcfce0975
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-05 13:34:19 +0000

    monty mod() stack3.c added

---
+++ stack3.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ *_mod -main entry.
+ *Description: module of the second top element of the stack by the top element
+ * @top: element at the top of the stack (head)
+ * @line_number: constant int value in the structure
+ * Return: void
+ **/
+void _mod(stack_t **top, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (*top == NULL || (*top)->next ==  NULL)
+		mod_error(line_number);
+
+	if ((*top)->n == 0)
+		div_error2(line_number);
+
+	tmp = (*top)->next;
+	tmp->n = (tmp->n) % (*top)->n;
+	pop_stack(top, line_number);
+}
