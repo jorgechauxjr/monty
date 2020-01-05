5c36be33a9831aa6cd7ab113def52cb3cb962f33
jorgechauxjr <chauxjorge@hotmail.com>
2020-01-05 00:06:37 +0000

    monty swap 1.1 just swap the numbers not the node

--- stack2.c
+++ stack2.c
@@ -8,19 +8,12 @@
  **/
 void _swap(stack_t **top, unsigned int line_number)
 {
-	stack_t *tmp, *two, *tree;
+	int num;
 
 	if (*top == NULL || (*top)->next == NULL)
 		swap_error(line_number);
 
-	tmp = *top;
-	two = tmp->next;
-	tree = tmp->next->next;
-
-	*top = two;
-	two->next = tmp;
-	two->prev = NULL;
-	tmp->prev = two;
-	tmp->next = tree;
-	tree->prev = tmp;
+	num = (*top)->n;
+	(*top)->n = (*top)->next->n;
+	(*top)->next->n = num;
 }
